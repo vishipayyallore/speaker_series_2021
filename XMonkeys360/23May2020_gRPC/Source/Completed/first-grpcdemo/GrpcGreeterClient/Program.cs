29e2abcc4764aeba2d0f795d910f299239777e41
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-05-23 08:24:16 +0530

    Initial Version

---
+++ XMonkeys360/23May2020_gRPC/Source/Completed/first-grpcdemo/GrpcGreeterClient/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Grpc.Net.Client;
+using GrpcGreater;
+using System.Threading.Tasks;
+
+namespace GrpcGreeterClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" });
+
+            Console.WriteLine("Greeting: " + reply.Message);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
