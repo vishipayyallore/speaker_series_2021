8b2f74b59ba93367fc1dc7dcf4560eeb6a6e84ad
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-05-26 21:56:18 +0530

    Inital Version

---
+++ XMonkeys360/30May2020_gRPC_Session2/Source/1Server/College.ApplicationCore/Constants/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace College.ApplicationCore.Constants
+{
+
+    public static class Constants
+    {
+        public static string ConnectionString { get; set; } = "ConnectionStrings:CollegeDBConnectionString";
+    }
+
+}
---
+++ XMonkeys360/30May2020_gRPC_Session2/Source/2ConsoleClient/CollegeGrpc.ConsoleClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CollegeGrpc.ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
