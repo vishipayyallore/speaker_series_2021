7f433b79dffd84fb0a245fae0b47d2e483163f61
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-06-19 23:25:44 +0530

    Initial Version

---
+++ XMonkeys360/gRPC_Session5_20Jun2020/Source/1Server/College.ApplicationCore/Interfaces/IProfessorBLL.cs
@@ -0,0 +1,17 @@
+﻿using College.ApplicationCore.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace College.ApplicationCore.Interfaces
+{
+
+    public interface IProfessorBLL
+    {
+        Professor AddProfessor(Professor professor);
+
+        IEnumerable<Professor> GetAllProfessors();
+
+        Professor GetProfessorById(Guid professorId);
+    }
+
+}
---
+++ brewingcodes/gRPC_Session1_20Jun2020/Source/first-grpcdemo/GrpcGreeterClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using Grpc.Net.Client;
+using GrpcGreeter;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcGreeterClient
+{
+
+    class Program
+    {
+
+        static async Task Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+
+            var reply = await client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" });
+            Console.WriteLine("Greeting: " + reply.Message);
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+    }
+
+}
