08dcc3ef9e2796fde74f0e727a314b947252ba78
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-06-20 10:31:00 +0530

    Few Changes

--- XMonkeys360/gRPC_Session5_20Jun2020/Source/8RestWebAPIgRPC/College.Microservice/Common/Constants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace College.Microservice.Common
-{
-
-    public static class Constants
-    {
-        public static string ConnectionString { get; set; } = "ConnectionStrings:CollegeDBConnectionString";
-    }
-
-}
--- XMonkeys360/gRPC_Session5_20Jun2020/Source/8RestWebAPIgRPC/College.Microservice/Startup.cs
+++ XMonkeys360/gRPC_Session5_20Jun2020/Source/8RestWebAPIgRPC/College.Microservice/Startup.cs
@@ -1,13 +1,18 @@
-﻿using College.Microservice.BAL;
-using College.Microservice.Common;
-using College.Microservice.Persistence;
+﻿using College.ApplicationCore.Constants;
+using College.ApplicationCore.Interfaces;
+using College.GrpcServer.Services;
+using College.Microservice.BAL;
+using College.ServerBLL;
+using College.ServerDAL;
 using College.Services.Persistence;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
 
 namespace College.Microservice
 {
@@ -24,15 +29,45 @@ namespace College.Microservice
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddGrpc();
+
             services.AddControllers();
 
+            // ***************** For REST End Points ***************** 
             // Adding EF Core
-            var connectionString = Configuration[Constants.ConnectionString];
-            services.AddDbContext<CollegeDbContext>(o => o.UseSqlServer(connectionString));
-
-            // Application Services
+            var connectionString = Configuration[Constants.DataStore.SqlConnectionString];
+            // services.AddDbContext<CollegeDbContext>(o => o.UseSqlServer(connectionString));
+            services.AddDbContext<Persistence.CollegeDbContext>(options =>
+            {
+                options.UseSqlServer(connectionString,
+                sqlServerOptionsAction: sqlOptions =>
+                {
+                    sqlOptions.EnableRetryOnFailure(
+                    maxRetryCount: 10,
+                    maxRetryDelay: TimeSpan.FromSeconds(60),
+                    errorNumbersToAdd: null);
+                });
+            });
+            
             services.AddScoped<ProfessorsBal>();
             services.AddScoped<ProfessorsDal>();
+            // ***************** For REST End Points ***************** 
+
+            // ***************** For gRPC End Points ***************** 
+            // Server DAL
+            services.AddDbContext<ServerDAL.Persistence.CollegeDbContext>(o => o.UseSqlServer(connectionString));
+
+            // College Application Services
+            services.AddScoped<IProfessorBLL, ProfessorBLL>();
+            services.AddScoped<IProfessorDAL, ProfessorDAL>();
+
+            // Adding Redis Cache 
+            services.AddStackExchangeRedisCache(option =>
+            {
+                option.Configuration = Configuration[Constants.DataStore.RedisConnectionString];
+                option.InstanceName = Constants.RedisCacheStore.InstanceName;
+            });
+            // ***************** For gRPC End Points ***************** 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,7 +86,15 @@ namespace College.Microservice
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapGrpcService<CollegeGrpcService>();
+
                 endpoints.MapControllers();
+
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+                });
+
             });
         }
     }
