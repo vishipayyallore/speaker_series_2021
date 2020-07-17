365703ad03698aeb98d9000fc4c22caf7fed4fc2
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-07-18 00:27:56 +0530

    Tested Web API as gRPC Client

--- BDotNet/gRPC-Blazor_18Jul2020/Source/3gRPCWeb/College.GrpcServer/Services/CollegeGrpcService.cs
+++ BDotNet/gRPC-Blazor_18Jul2020/Source/3gRPCWeb/College.GrpcServer/Services/CollegeGrpcService.cs
@@ -1,9 +1,12 @@
 ﻿using College.Core.Entities;
 using College.Core.Interfaces;
 using College.GrpcServer.Protos;
+using Google.Protobuf.WellKnownTypes;
 using Grpc.Core;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
 using static College.GrpcServer.Protos.CollegeService;
 
@@ -48,6 +51,63 @@ namespace College.GrpcServer.Services
 
             return Task.FromResult(newProfessor);
         }
+
+        public override async Task<AllProfessorsResonse> GetAllProfessors(Empty request, ServerCallContext context)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            AllProfessorsResonse allProfessorsResonse = new AllProfessorsResonse();
+
+            stopwatch.Start();
+
+            var allProfessors = _professorsBll.GetAllProfessors();
+
+            stopwatch.Stop();
+            _logger.Log(LogLevel.Warning, $"Time Taken to Retireve a Record: {stopwatch.ElapsedMilliseconds} ms");
+
+            allProfessorsResonse.Count = allProfessors.Count();
+
+            foreach (var professor in allProfessors)
+            {
+                // TODO: Remove Technical Debt
+                allProfessorsResonse.Professors.Add(GetProfessorObject(professor));
+            }
+
+            return await Task.FromResult(allProfessorsResonse);
+        }
+
+        public override async Task<GetProfessorResponse> GetProfessorById(GetProfessorRequest request, ServerCallContext context)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            Professor professor;
+
+            stopwatch.Start();
+
+            // Going to Data Store SQL Server
+            professor = _professorsBll.GetProfessorById(Guid.Parse(request.ProfessorId));
+
+            stopwatch.Stop();
+            _logger.Log(LogLevel.Warning, $"Time Taken to Retireve a Record: {stopwatch.ElapsedMilliseconds} ms");
+
+            GetProfessorResponse getProfessorResponse = new GetProfessorResponse();
+            getProfessorResponse = GetProfessorObject(professor);
+
+            return await Task.FromResult(getProfessorResponse);
+        }
+
+        //====================== Private Methods ======================
+        private static GetProfessorResponse GetProfessorObject(Professor professor)
+        {
+            return new GetProfessorResponse()
+            {
+                ProfessorId = professor.ProfessorId.ToString(),
+                Name = professor.Name,
+                Teaches = professor.Teaches,
+                IsPhd = professor.IsPhd,
+                Salary = decimal.ToDouble(professor.Salary),
+                Doj = Timestamp.FromDateTime(professor.Doj.ToUniversalTime())
+            };
+        }
+        //====================== Private Methods ======================
 
     }
 
--- BDotNet/gRPC-Blazor_18Jul2020/Source/5gRPCClients/CollegeGrpc.WebApiClient/Controllers/ProfessorsController.cs
+++ BDotNet/gRPC-Blazor_18Jul2020/Source/5gRPCClients/CollegeGrpc.WebApiClient/Controllers/ProfessorsController.cs
@@ -6,6 +6,10 @@ using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
 using static College.GrpcServer.Protos.CollegeService;
 using Google.Protobuf.WellKnownTypes;
+using System.Collections.Generic;
+using College.Core.Entities;
+using System.Linq;
+using System;
 
 namespace CollegeGrpc.WebApiClient.Controllers
 {
@@ -31,12 +35,65 @@ namespace CollegeGrpc.WebApiClient.Controllers
         {
             _logger.Log(LogLevel.Debug, "Request Received for ProfessorsController::AddProfessor");
 
-            var results = await _collegeServiceClient.AddProfessorAsync(professor);
+            var createdProfessor = await _collegeServiceClient.AddProfessorAsync(professor);
 
             _logger.Log(LogLevel.Debug, "Sending Response from ProfessorsController::AddProfessor");
 
-            return Created(string.Empty, results);
+            return Created(string.Empty, createdProfessor);
         }
 
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Professor>>> Get()
+        {
+            IList<Professor> professors = new List<Professor>();
+
+            _logger.Log(LogLevel.Debug, "Request Received for ProfessorsController::Get");
+
+            // Going to Data Store SQL Server
+            var allProfessors = await _collegeServiceClient.GetAllProfessorsAsync(new Empty());
+
+            foreach (var prof in allProfessors.Professors)
+            {
+                professors.Add(GetProfessor(prof));
+            }
+
+            _logger.Log(LogLevel.Debug, "Returning the results from ProfessorsController::Get");
+
+            return Ok(professors);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Professor>> GetProfessorById(Guid professorId)
+        {
+            _logger.Log(LogLevel.Debug, "Request Received for ProfessorsController::Get");
+
+            var professorRequest = new GetProfessorRequest { ProfessorId = professorId.ToString() };
+
+            var _professor = await _collegeServiceClient.GetProfessorByIdAsync(professorRequest);
+
+            if (_professor == null)
+            {
+                return NotFound();
+            }
+
+            _logger.Log(LogLevel.Debug, "Returning the results from ProfessorsController::Get");
+
+            return Ok(GetProfessor(_professor));
+        }
+
+        //====================== Private Methods ======================
+        private Professor GetProfessor(GetProfessorResponse prof)
+        {
+            return new Professor
+            {
+                ProfessorId = Guid.Parse(prof.ProfessorId),
+                Name = prof.Name,
+                Doj = prof.Doj.ToDateTime(),
+                Teaches = prof.Teaches,
+                Salary = Convert.ToDecimal(prof.Salary),
+                IsPhd = prof.IsPhd
+            };
+        }
+        //====================== Private Methods ======================
     }
 }
