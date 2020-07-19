df80a1d9879d977528466f3f06cf9a3810128b34
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-07-19 22:24:13 +0530

    Few Changes

--- brewingcodes/webapi-session1_18-Jul-2020/College.WebApp/src/app/app-routing.module.ts
+++ brewingcodes/webapi-session1_18-Jul-2020/College.WebApp/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: '**', redirectTo: 'pagenotfound', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- brewingcodes/webapi-session1_18-Jul-2020/College.WebApp/src/app/app.module.ts
+++ brewingcodes/webapi-session1_18-Jul-2020/College.WebApp/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopNavbarComponent } from './components/top-navbar/top-navbar.component';
 import { SideNavbarComponent } from './components/side-navbar/side-navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         TopNavbarComponent,
-        SideNavbarComponent
+        SideNavbarComponent,
+        FooterComponent,
+        DashboardComponent
     ],
     imports: [
         BrowserModule,
