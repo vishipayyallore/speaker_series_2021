4ddf4405e5222c06b4f5acb6c0fc357c2c58fa65
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-07-25 16:20:57 +0530

    Initial Version

---
+++ brewingcodes/webapi-session2_25-Jul-2020/Source/College.Core/Constants/Constants.cs
@@ -0,0 +1,25 @@
+﻿namespace College.Core.Constants
+{
+
+    public static class Constants
+    {
+
+        public static class DataStore
+        {
+            public static string SqlConnectionString { get; } = "ConnectionStrings:CollegeDBConnectionString";
+
+            public static string RedisConnectionString { get; } = "ConnectionStrings:CollegeRedisConnectionString";
+        }
+
+        public static class RedisCacheStore
+        {
+            public static string InstanceName { get; } = "CollegeRedisCache_";
+
+            public static string AllProfessorsKey { get; } = "college_all_professors";
+
+            public static string SingleProfessorsKey { get; } = "professor_";
+        }
+
+    }
+
+}
---
+++ brewingcodes/webapi-session2_25-Jul-2020/Source/College.WebAPI/Controllers/ProfessorsController.cs
@@ -0,0 +1,60 @@
+﻿using College.Core.Entities;
+using College.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace College.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfessorsController : ControllerBase
+    {
+
+        private readonly ILogger<ProfessorsController> _logger;
+        private readonly IProfessorsBLL _professorsBLL;
+
+        public ProfessorsController(ILogger<ProfessorsController> logger, IProfessorsBLL professorsBLL)
+        {
+            _logger = logger;
+
+            _professorsBLL = professorsBLL;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Professor>>> Get()
+        {
+            IEnumerable<Professor> professors;
+
+            _logger.Log(LogLevel.Debug, "Request Received for ProfessorsController::Get");
+
+            professors = await _professorsBLL.GetAllProfessors();
+
+            _logger.Log(LogLevel.Debug, "Returning the results from ProfessorsController::Get");
+
+            return Ok(professors);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Professor>> GetProfessorById(Guid id)
+        {
+            Professor professor;
+
+            _logger.Log(LogLevel.Debug, "Request Received for ProfessorsController::Get");
+
+            professor = await _professorsBLL.GetProfessorById(id);
+
+            if (professor == null)
+            {
+                return NotFound();
+            }
+
+            _logger.Log(LogLevel.Debug, "Returning the results from ProfessorsController::Get");
+
+            return Ok(professor);
+        }
+
+    }
+}
