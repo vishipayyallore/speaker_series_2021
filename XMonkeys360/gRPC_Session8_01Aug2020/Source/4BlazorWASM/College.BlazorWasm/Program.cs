8f853bb5786e6259476f6d166e3b06b1889f70bb
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-08-01 10:40:13 +0530

    Fully Working Version

---
+++ XMonkeys360/gRPC_Session8_01Aug2020/Source/4BlazorWASM/College.BlazorWasm/Program.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Grpc.Net.Client.Web;
+using Grpc.Net.Client;
+
+namespace College.BlazorWasm
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddSingleton(services =>
+            {
+                // Get the service address from appsettings.json
+                var config = services.GetRequiredService<IConfiguration>();
+                var backendUrl = config["RPCService:ServiceUrl"];
+
+                // Create a channel with a GrpcWebHandler that is 
+                // addressed to the backend server.
+                // GrpcWebText is used because server streaming requires it. If server streaming is not used in your app
+                // then GrpcWeb is recommended because it produces smaller messages.
+                var httpHandler = new GrpcWebHandler(GrpcWebMode.GrpcWeb, new HttpClientHandler());
+
+                return GrpcChannel.ForAddress(backendUrl, new GrpcChannelOptions { HttpHandler = httpHandler });
+            });
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
