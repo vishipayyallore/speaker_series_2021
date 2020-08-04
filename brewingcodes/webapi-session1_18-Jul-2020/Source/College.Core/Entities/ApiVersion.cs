026a16d3d6797ea75b9efe77dfff9c1292f8c8a4
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-08-04 14:13:11 +0530

    Few Changes

---
+++ brewingcodes/webapi-session1_18-Jul-2020/Source/College.Core/Entities/ApiVersion.cs
@@ -0,0 +1,12 @@
+﻿namespace College.Core.Entities
+{
+    public class ApiVersion
+    {
+        public string AssemblyVersion { get; set; }
+
+        public string FileVersion { get; set; }
+
+        public string ProductVersion { get; set; }
+    }
+
+}
---
+++ brewingcodes/webapi-session1_18-Jul-2020/Source/College.WebAPI/Controllers/ApiVersionController.cs
@@ -0,0 +1,28 @@
+﻿using College.Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace College.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiVersionController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var apiVersion = new ApiVersion()
+            {
+                AssemblyVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString(),
+                FileVersion = FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).FileVersion,
+                ProductVersion = FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).ProductVersion
+            };
+
+            return Ok(apiVersion);
+        }
+
+    }
+
+}
