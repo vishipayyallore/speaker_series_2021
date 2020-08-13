f8ec205cd31ba0df5c77c3309db0bc13b68bb74a
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-08-13 18:33:20 +0530

    Few Changes

---
+++ XMonkeys360/gRPC_Session9_15Aug2020/Source/1Server/College.ApplicationCore/Constants/Constants.cs
@@ -0,0 +1,19 @@
+﻿namespace College.ApplicationCore.Constants
+{
+
+    public static class Constants
+    {
+
+        public static class SQLDataStore
+        {
+            public static string ConnectionString { get; set; } = "ConnectionStrings:CollegeDBConnectionString";
+        }
+
+        public static class AddressConstants
+        {
+            public static string[] EnrollmentStatus = { "Pending", "In Progress", "Rejected", "Enrolled" };
+        }
+
+    }
+
+}
---
+++ XMonkeys360/gRPC_Session9_15Aug2020/Source/1Server/College.ApplicationCore/Interfaces/IProfessorDAL.cs
@@ -0,0 +1,11 @@
+﻿using College.ApplicationCore.Entities;
+
+namespace College.ApplicationCore.Interfaces
+{
+
+    public interface IProfessorDAL
+    {
+        Professor AddProfessor(Professor professor);
+    }
+
+}
