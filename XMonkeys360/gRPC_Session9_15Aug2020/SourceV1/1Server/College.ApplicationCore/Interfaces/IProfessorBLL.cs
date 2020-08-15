c3a96bcf51b6ab04341a01dfc82384ee1e88b845
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-08-15 09:03:32 +0530

    Initial Version

---
+++ XMonkeys360/gRPC_Session9_15Aug2020/SourceV1/1Server/College.ApplicationCore/Interfaces/IProfessorBLL.cs
@@ -0,0 +1,11 @@
+﻿using College.ApplicationCore.Entities;
+
+namespace College.ApplicationCore.Interfaces
+{
+
+    public interface IProfessorBLL
+    {
+        Professor AddProfessor(Professor professor);
+    }
+
+}
---
+++ XMonkeys360/gRPC_Session9_15Aug2020/SourceV1/1Server/College.ServerBLL/ProfessorBLL.cs
@@ -0,0 +1,28 @@
+﻿using College.ApplicationCore.Entities;
+using College.ApplicationCore.Interfaces;
+using Microsoft.Extensions.Logging;
+
+namespace College.ServerBLL
+{
+
+    public class ProfessorBLL : IProfessorBLL
+    {
+        private readonly IProfessorDAL _professorDal;
+        private readonly ILogger<ProfessorBLL> _logger;
+
+        public ProfessorBLL(IProfessorDAL professorDal, ILogger<ProfessorBLL> logger)
+        {
+            _professorDal = professorDal;
+
+            _logger = logger;
+        }
+
+        public Professor AddProfessor(Professor professor)
+        {
+            _logger.Log(LogLevel.Debug, "Request Received for ProfessorBLL::AddProfessor");
+
+            return _professorDal.AddProfessor(professor);
+        }
+    }
+
+}
