433a0c4f932104e2f4cc22722a21e1091dd46a74
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-08-15 12:36:48 +0530

    Initial Version

---
+++ talklify/Rest_gRPC_Blazor_WASM_15Aug2020/README.md
@@ -0,0 +1,2 @@
+# Speaking Event - BDotNet (gRPC Web + Blazor WASM)
+This Repository to hold the Demos for Speaker Series
---
+++ talklify/Rest_gRPC_Blazor_WASM_15Aug2020/Source/1Common/College.Core/Interfaces/IProfessorsBLL.cs
@@ -0,0 +1,17 @@
+﻿using College.Core.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace College.Core.Interfaces
+{
+
+    public interface IProfessorsBLL
+    {
+        Professor AddProfessor(Professor professor);
+
+        IEnumerable<Professor> GetAllProfessors();
+
+        Professor GetProfessorById(Guid professorId);
+    }
+
+}
