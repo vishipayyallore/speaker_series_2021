f8221682cd22f96a7ccffec67d4badc85d4be0d5
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-09-03 12:24:41 +0530

    Initial Version

---
+++ General/SimpleRestSharpDemo/SimpleRestSharpDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using RestSharp;
+using System;
+using System.IO;
+using static System.Console;
+
+namespace SimpleRestSharpDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var restSharpClient = new RestClient("https://www.otcmarkets.com");
+
+            var downloadRequest = new RestRequest("research/stock-screener/api/downloadCSV?pageSize=20", 
+                Method.GET);
+
+            var fileInBytes = restSharpClient.DownloadData(downloadRequest);
+
+            File.WriteAllBytes(@"D:\LordKrishna\GitHub\speaker_series_2020\General\SimpleRestSharpDemo\SimpleRestSharpDemo\Downloads\StockData.xlxs",
+                fileInBytes);
+
+            WriteLine("\n\nPress any key ...");
+            ReadKey();
+        }
+    }
+}
