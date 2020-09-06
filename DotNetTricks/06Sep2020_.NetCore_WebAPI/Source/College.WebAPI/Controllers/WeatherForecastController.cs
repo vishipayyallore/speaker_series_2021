a4bfede6d7654929ebedf21fa5576731c6a03863
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-09-06 18:17:45 +0530

    Slight Changes

--- DotNetTricks/06Sep2020_.NetCore_WebAPI/Source/College.WebAPI/Controllers/WeatherForecastController.cs
+++ DotNetTricks/06Sep2020_.NetCore_WebAPI/Source/College.WebAPI/Controllers/WeatherForecastController.cs
@@ -1,15 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using College.WebAPI.Entities;
+﻿using College.WebAPI.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace College.WebAPI.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
--- DotNetTricks/06Sep2020_.NetCore_WebAPI/Source/College.WebAPI/DAL/Persistence/CollegeCosmosDbContext.cs
+++ DotNetTricks/06Sep2020_.NetCore_WebAPI/Source/College.WebAPI/DAL/Persistence/CollegeCosmosDbContext.cs
@@ -14,15 +14,6 @@ namespace College.WebAPI.DAL.Persistence
 
         public DbSet<Student> Students { get; set; }
 
-        #region Configuration
-        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //    => optionsBuilder.UseCosmos(
-        //            "https://localhost:8081",
-        //            "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==",
-        //            databaseName: "College")
-        //    .EnableSensitiveDataLogging(true);
-        #endregion
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             #region DefaultContainer
