841b2a4f6c617e43de9c180fe35da9553e55b56c
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-09-20 11:20:41 +0530

    Initial Version

---
+++ CSharpCorner/19Sep2020_gRPCWeb/Source/1Common/ClientApps.Common/Utilities/RandomNumberGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ClientApps.Common.Utilities
+{
+
+    public static class RandomNumberGenerator
+    {
+        static readonly Random _random = new Random();
+
+        static public int GetRandomValue(int start = 8, int end = 50)
+        {
+            return _random.Next(start, end);
+        }
+    }
+
+}
---
+++ CSharpCorner/19Sep2020_gRPCWeb/Source/2RestWebAPI/College.WebAPI/Controllers/ProfessorsController.cs
@@ -0,0 +1,42 @@
+﻿using College.Core.Entities;
+using College.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace College.WebAPI.Controllers
+{
+
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ProfessorsController : ControllerBase
+    {
+
+        private readonly IProfessorsBLL _professorsBLL;
+        private readonly ILogger<ProfessorsController> _logger;
+
+        public ProfessorsController(IProfessorsBLL professorsBLL, ILogger<ProfessorsController> logger)
+        {
+            _professorsBLL = professorsBLL;
+
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Professor>> Get()
+        {
+            IEnumerable<Professor> professors;
+
+            _logger.Log(LogLevel.Debug, "Request Received for ProfessorsController::Get");
+
+            // Going to Data Store SQL Server
+            professors = _professorsBLL.GetAllProfessors();
+
+            _logger.Log(LogLevel.Debug, "Returning the results from ProfessorsController::Get");
+
+            return Ok(professors);
+        }
+
+    }
+
+}
