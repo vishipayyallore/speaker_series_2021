f9d957bc07ab00b3daeb6c12f88a9feb9b345744
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-10-11 15:18:06 +0530

    Integrated with Console and Windows Forms client

--- TechPlatform/gRPC_Session2_11Oct2020/Source/3ConsoleClient/CollegeGrpc.ConsoleClient/Program.cs
+++ TechPlatform/gRPC_Session2_11Oct2020/Source/3ConsoleClient/CollegeGrpc.ConsoleClient/Program.cs
@@ -1,12 +1,55 @@
-﻿using System;
+﻿using ClientApps.Common.Utilities;
+using College.GrpcServer.Protos;
+using Google.Protobuf.WellKnownTypes;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+using static System.Console;
 
 namespace CollegeGrpc.ConsoleClient
 {
     class Program
     {
-        static void Main(string[] args)
+        private static IConfiguration _config;
+
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string response = "Y";
+            _config = new ConfigurationBuilder()
+                            .SetBasePath(Directory.GetCurrentDirectory())
+                            .AddJsonFile("appsettings.json").Build();
+
+            var _client = CollegeServiceClientHelper.GetCollegeServiceClient(_config["RPCService:ServiceUrl"]);
+
+            WriteLine("\n\nCreating New Professor ...");
+            while (response == "Y")
+            {
+                // Add New Professor
+                NewProfessorRequest professorNew = GenerateNewProfessor();
+
+                var newlyAddedProfessor = await _client.AddProfessorAsync(professorNew);
+                WriteLine($"\n\nNew Professor Added with Professor Id: {newlyAddedProfessor.ProfessorId}");
+
+                WriteLine("\n\nDo you want to create New Professor: {Y/N}");
+                response = ReadKey().KeyChar.ToString().ToUpper();
+            }
+
+            Console.WriteLine("\n\nThank You for using the application. \n\nPress any key ...");
+            Console.ReadKey();
+        }
+
+        private static NewProfessorRequest GenerateNewProfessor()
+        {
+            return new NewProfessorRequest()
+            {
+                Name = NameGenerator.GenerateName(12),
+                Doj = Timestamp.FromDateTime(DateTime.Now.AddYears(-1 * RandomNumberGenerator.GetRandomValue(1, 10)).ToUniversalTime()),
+                Teaches = "CSharp, Java",
+                Salary = 1234.56,
+                IsPhd = true
+            };
         }
     }
 }
--- TechPlatform/gRPC_Session2_11Oct2020/Source/4WindowsFormClient/CollegeGrpc.WindowsFormClient/Program.cs
+++ TechPlatform/gRPC_Session2_11Oct2020/Source/4WindowsFormClient/CollegeGrpc.WindowsFormClient/Program.cs
@@ -17,7 +17,7 @@ namespace CollegeGrpc.WindowsFormClient
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormAddNewProfessor());
         }
     }
 }
