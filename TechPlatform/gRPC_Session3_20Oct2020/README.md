beaddfd52f6f44e984ec4885d19a36dda93c0a17
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-10-18 22:14:36 +0530

    Initial Version

---
+++ TechPlatform/gRPC_Session3_20Oct2020/README.md
@@ -0,0 +1,5 @@
+# Tech Platform Speaker Event
+
+## This Repository to holds the Demos for gRPC with .Net Core Session 3
+
+
---
+++ TechPlatform/gRPC_Session3_20Oct2020/Source/1Common/College.Cache.DAL/Persistence/RedisCacheDbContext.cs
@@ -0,0 +1,19 @@
+﻿using College.Core.Interfaces;
+using StackExchange.Redis;
+
+namespace College.Cache.DAL.Persistence
+{
+
+    public class RedisCacheDbContext : IRedisCacheDbContext
+    {
+
+        public RedisCacheDbContext(ConnectionMultiplexer connectionMultiplexer)
+        {
+            RedisDatabase = connectionMultiplexer.GetDatabase();
+        }
+
+        public IDatabase RedisDatabase { get; }
+
+    }
+
+}
