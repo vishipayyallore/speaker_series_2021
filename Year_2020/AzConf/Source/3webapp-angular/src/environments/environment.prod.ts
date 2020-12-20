a2de95264defe09e81a0ce0448c3051bce50479c
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-12-20 22:33:13 +0530

    Moved into Year_2020 Folder

---
+++ Year_2020/AzConf/Source/3webapp-angular/src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  webApiUrl: "https://app-college-azconf-stage.azurewebsites.net/api/v1",
+  appInsights: {
+    instrumentationKey: '82fe3a54-0515-4ea7-947b-a8822d813f24'
+  }
+};
