a3b327846c15dd8131d961df53b72b2625c84991
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-03-28 09:58:26 +0530

    Initial Version

---
+++ CSharpCorner/gRPC_Session10_28Mar2021/README.md
@@ -0,0 +1,54 @@
+# C# Corner Speaker Event
+
+> 1. Event Date: **28-Mar-2021**
+> 1. Event URL: [C# Corner](https://www.c-sharpcorner.com/events/working-with-authenticate-and-authorization)
+
+![gRPC Authentication and Authorization |100x100](./Documentation/Images/ViswanathaSwamy.PNG)
+
+## This Repository to holds the Demos for gRPC with .Net Core Session 10
+
+### Service to Service Communication
+
+### Authentication
+https://docs.microsoft.com/en-us/aspnet/core/security/authentication/?view=aspnetcore-5.0
+
+### Authorization
+https://docs.microsoft.com/en-us/aspnet/core/security/authorization/introduction?view=aspnetcore-5.0
+
+### Third Party Providers
+Facebook | Google | Twitter
+
+### oAuth
+
+> 1. https://oauth.net/2/
+> 1. https://oauth.net/2/grant-types/
+> 1. https://oauth.net/2/grant-types/client-credentials/
+
+
+> 1. https://developer.okta.com/docs/guides/validate-id-tokens/overview/
+
+### Open Id Connect
+> 1. https://openid.net/connect/
+
+### Identity Server
+> 1. https://identityserver4.readthedocs.io/en/latest/
+
+### Azure Active Directory (Azure AD)
+https://docs.microsoft.com/en-us/azure/active-directory/fundamentals/active-directory-whatis#:~:text=Azure%20Active%20Directory%20(Azure%20AD,in%20and%20access%20resources%20in%3A&text=Internal%20resources%2C%20such%20as%20apps,developed%20by%20your%20own%20organization.
+
+### Identity as Service (Auth0 | Okta)
+
+> 1. https://auth0.com/
+
+***********************************************************************************************************************************
+
+### Retrieve Authentication Token using CURL
+![Auth Token | 100x100](./Documentation/Images/Curl_Auth_Token.PNG)
+
+### Retrieve Authentication Token using Postman
+![Auth Token 1 | 100x100](./Documentation/Images/Post_Auth_Token_1.PNG)
+
+![Auth Token 2 | 100x100](./Documentation/Images/Post_Auth_Token_1.PNG)
+
+> 1. Parameter the Client Id, Client Secret, and Audience
+> View the Token in JWT.io
---
+++ CSharpCorner/gRPC_Session10_28Mar2021/Source/3gRPCClients/1ConsoleClient/CollegeGrpc.ConsoleClient/Program.cs
@@ -0,0 +1,142 @@
+﻿using ClientApps.Common.Utilities;
+using College.GrpcServer.Protos;
+using CollegeGrpc.ConsoleClient.Helpers;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using static College.Core.Constants.Constants;
+using static System.Console;
+
+namespace CollegeGrpc.ConsoleClient
+{
+    class Program
+    {
+        private static IConfiguration _config;
+        private static string _header = "======================================================================";
+        private static string _footer = "----------------------------------------------------------------------";
+
+        static async Task Main(string[] args)
+        {
+            _config = new ConfigurationBuilder()
+                            .SetBasePath(Directory.GetCurrentDirectory())
+                            .AddJsonFile("appsettings.json").Build();
+
+            var _collegeClient = CollegeServiceClientHelper.GetCollegeServiceClient(_config["RPCService:ServiceUrl"]);
+
+            //var _addressClient = AddressServiceClientHelper.GetAddressBookServerClient(_config["RPCService:ServiceUrl"]);
+
+            ////**************** ADDRESS BOOK CLIENT SIDE STREAMING ****************//
+            //var userAddress = new AddAddressRequest
+            //{
+            //    StudentId = Guid.NewGuid().ToString(), /* To Be replaced with Students's Id*/
+            //    Name = NameGenerator.GenerateName(12),
+            //    FullAddress = AddressGenerator.GenerateAddress(),
+            //    Enrollment = AddressConstants.EnrollmentTypes[RandomNumberGenerator.GetRandomValue(1, 4)]
+            //};
+
+            //using (var stream = _addressClient.AddAddressEnrollments())
+            //{
+            //    foreach (string enrollmentType in AddressConstants.EnrollmentTypes)
+            //    {
+            //        userAddress.Enrollment = enrollmentType;
+            //        await stream.RequestStream.WriteAsync(userAddress).ConfigureAwait(false);
+            //    }
+
+            //    await stream.RequestStream.CompleteAsync().ConfigureAwait(false);
+
+            //    await stream;
+            //    WriteLine($"Sent All");
+            //}
+            ////**************** ADDRESS BOOK CLIENT SIDE STREAMING ****************//
+
+            ////**************** ADDRESS BOOK SERVER SIDE STREAMING ****************//
+            //var studentEnrollmentsRequest = new AddressEnrollmentRequest 
+            //{ 
+            //    StudentId = "62b76fc7-46d9-4d1c-a246-7f4083a87634"
+            //};
+
+            //using var studentEnrollments = _addressClient
+            //                                    .RetrieveAddressEnrollments(studentEnrollmentsRequest);
+            //try
+            //{
+            //    await foreach (var studentEnrollment in studentEnrollments.ResponseStream.ReadAllAsync())
+            //    {
+            //        WriteLine($"{studentEnrollment.StudentId} | {studentEnrollment.Name} | {studentEnrollment.Enrollment} | {studentEnrollment.EnrollmentStatus}");
+            //    }
+            //}
+            //catch (RpcException ex) when (ex.StatusCode == StatusCode.Cancelled)
+            //{
+            //    Console.WriteLine("Stream cancelled.");
+            //}
+            ////**************** ADDRESS BOOK SERVER SIDE STREAMING ****************//
+
+            /*
+            DisplayHeader("Creating New Professor ...");
+            NewProfessorRequest professorNew = GenerateNewProfessor();
+            var newlyAddedProfessor = await _collegeClient.AddProfessorAsync(professorNew);
+            WriteLine($"\n\nNew Professor Added with Professor Id: {newlyAddedProfessor.ProfessorId}");
+            DisplayFooter();
+            */
+
+            //DisplayHeader("Retrieve Single Row ...");
+            WriteLine("\n\nPlease enter a Professor Id: ");
+            var professorId = ReadLine();
+
+            var professorRequest = new GetProfessorRequest { ProfessorId = professorId };
+            var professor = await _collegeClient.GetProfessorByIdAsync(professorRequest);
+
+            DisplayProfessorDetails(professor);
+            DisplayFooter();
+
+            DisplayHeader("Retrieve All Rows ...");
+            var professors = await _collegeClient.GetAllProfessorsAsync(new Empty());
+
+            foreach (var prof in professors.Professors)
+            {
+                DisplayProfessorDetails(prof);
+            }
+
+            WriteLine("\n\nThank You for using the application. \n\nPress any key ...");
+            ReadKey();
+        }
+
+        // ******************** Private Methods ********************
+        private static NewProfessorRequest GenerateNewProfessor()
+        {
+            return new NewProfessorRequest()
+            {
+                Name = NameGenerator.GenerateName(12),
+                Doj = Timestamp.FromDateTime(DateTime.Now.AddYears(-1 * RandomNumberGenerator.GetRandomValue(1, 10)).ToUniversalTime()),
+                Teaches = "CSharp, Java",
+                Salary = 1234.56,
+                IsPhd = true
+            };
+        }
+
+        private static void DisplayFooter()
+        {
+            WriteLine($"\n{_footer}\n");
+        }
+
+        private static void DisplayHeader(string headerTitle)
+        {
+            WriteLine($"\n\n{_header}");
+            WriteLine(headerTitle.PadLeft(25));
+            WriteLine(_header);
+        }
+
+        private static void DisplayProfessorDetails(GetProfessorResponse professor)
+        {
+            DisplayHeader("Professor Details");
+
+            WriteLine($"Name: {professor.Name} \nSalary: {professor.Salary} \nTeaches: {professor.Teaches} \nDOJ: {professor.Doj}");
+
+            DisplayFooter();
+        }
+        // ******************** Private Methods ********************
+
+    }
+}
