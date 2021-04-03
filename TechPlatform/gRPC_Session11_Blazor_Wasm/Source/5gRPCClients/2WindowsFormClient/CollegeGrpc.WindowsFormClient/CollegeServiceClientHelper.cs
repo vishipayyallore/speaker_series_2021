38130dee29a9b8772cb6eaa8083f627e92995627
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-04-03 11:18:29 +0530

    After the Session

---
+++ TechPlatform/gRPC_Session11_Blazor_Wasm/Source/5gRPCClients/2WindowsFormClient/CollegeGrpc.WindowsFormClient/CollegeServiceClientHelper.cs
@@ -0,0 +1,24 @@
+﻿using Grpc.Net.Client;
+using static College.GrpcServer.Protos.CollegeSvc;
+
+namespace CollegeGrpc.WindowsFormClient
+{
+
+    public class CollegeServiceClientHelper
+    {
+        static private CollegeSvcClient _client;
+
+        static public CollegeSvcClient GetCollegeServiceClient(string serviceUrl)
+        {
+            if (_client == null)
+            {
+                var channel = GrpcChannel.ForAddress(serviceUrl);
+                _client = new CollegeSvcClient(channel);
+            }
+
+            return _client;
+        }
+
+    }
+
+}
