a770bb4aa1fa0c6553ccc72782168b10f6d0a5e5
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-04-11 21:35:16 +0530

    Few Changes

--- AzConf/17Apr2021_HandsOnAzureFunctions/Source/ngwebapp/src/app/app-routing.module.ts
+++ AzConf/17Apr2021_HandsOnAzureFunctions/Source/ngwebapp/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: '**', redirectTo: 'pagenotfound', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- AzConf/17Apr2021_HandsOnAzureFunctions/Source/ngwebapp/src/app/app.module.ts
+++ AzConf/17Apr2021_HandsOnAzureFunctions/Source/ngwebapp/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopNavbarComponent } from './components/shared/top-navbar/top-navbar.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { PageNotfoundComponent } from './components/shared/page-notfound/page-notfound.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavbarComponent,
+    FooterComponent,
+    PageNotfoundComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
