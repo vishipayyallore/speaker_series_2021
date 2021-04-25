8d82891b4f241d76dbe83bb980f94ad3b42033ca
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-04-25 23:30:58 +0530

    ..

---
+++ CSharpCorner/24Apr_books-store-blazorwasm-dapper/Source/BooksStoreData/dbo/Programmability/usp_add_book.sql
@@ -0,0 +1,21 @@
+﻿CREATE PROCEDURE [dbo].[usp_add_book]
+	@PictureUrl VARCHAR(100),
+	@Title VARCHAR(100),
+	@Author VARCHAR(100),
+	@IsActive BIT,
+	@ISBN VARCHAR(50),
+	@Pages NUMERIC(6,0)
+
+AS
+BEGIN
+	
+	SET NOCOUNT ON;
+
+	INSERT INTO Books
+		(PictureUrl, Title, Author, IsActive, ISBN, Pages)
+	VALUES
+		(@PictureUrl, @Title, @Author, @IsActive, @ISBN, @Pages);
+
+	SELECT SCOPE_IDENTITY();
+
+END
---
+++ CSharpCorner/24Apr_books-store-blazorwasm-dapper/Source/BooksStoreData/dbo/Programmability/usp_delete_book_by_id.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE [dbo].[usp_delete_book_by_id]
+	@Id int
+
+AS
+BEGIN
+	
+	SET NOCOUNT ON;
+
+	DELETE 
+	FROM
+		Books
+	WHERE
+		Id = @Id;
+
+	SELECT @@ROWCOUNT;
+
+END
