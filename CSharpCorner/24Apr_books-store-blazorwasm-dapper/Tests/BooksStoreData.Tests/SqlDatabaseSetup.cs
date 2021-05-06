cbf5a518af4dea909fdb4e982964e6a80a3165b1
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-05-06 07:33:26 +0530

    Added Unit Tests

---
+++ CSharpCorner/24Apr_books-store-blazorwasm-dapper/Tests/BooksStoreData.Tests/SqlDatabaseSetup.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Data.Tools.Schema.Sql.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+
+namespace BooksStoreData.Tests
+{
+    [TestClass()]
+    public class SqlDatabaseSetup
+    {
+
+        [AssemblyInitialize()]
+        public static void InitializeAssembly(TestContext ctx)
+        {
+            // Setup the test database based on setting in the
+            // configuration file
+            SqlDatabaseTestClass.TestService.DeployDatabaseProject();
+            SqlDatabaseTestClass.TestService.GenerateData();
+        }
+
+    }
+}
