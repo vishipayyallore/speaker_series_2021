16acee2241308d2e90a0b413e1a6486ecca2c8fa
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-05-15 14:39:17 +0530

    Before Session

---
+++ DotNetLearnersHouse/15May_books-store-blazorwasm-dapper/Tests/BooksStoreData.Tests/usp_add_book.tests.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.Data.Tools.Schema.Sql.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BooksStoreData.Tests
+{
+    [TestClass()]
+    public class UspAddBookTests : SqlDatabaseTestClass
+    {
+
+        public UspAddBookTests()
+        {
+            InitializeComponent();
+        }
+
+        [TestInitialize()]
+        public void TestInitialize()
+        {
+            base.InitializeTest();
+        }
+
+        [TestCleanup()]
+        public void TestCleanup()
+        {
+            base.CleanupTest();
+        }
+
+        #region Designer support code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Microsoft.Data.Tools.Schema.Sql.UnitTesting.SqlDatabaseTestAction dbo_usp_add_bookTest_TestAction;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UspAddBookTests));
+            Microsoft.Data.Tools.Schema.Sql.UnitTesting.Conditions.NotEmptyResultSetCondition notEmptyResultSetCondition1;
+            this.dbo_usp_add_bookTestData = new Microsoft.Data.Tools.Schema.Sql.UnitTesting.SqlDatabaseTestActions();
+            dbo_usp_add_bookTest_TestAction = new Microsoft.Data.Tools.Schema.Sql.UnitTesting.SqlDatabaseTestAction();
+            notEmptyResultSetCondition1 = new Microsoft.Data.Tools.Schema.Sql.UnitTesting.Conditions.NotEmptyResultSetCondition();
+            // 
+            // dbo_usp_add_bookTest_TestAction
+            // 
+            dbo_usp_add_bookTest_TestAction.Conditions.Add(notEmptyResultSetCondition1);
+            resources.ApplyResources(dbo_usp_add_bookTest_TestAction, "dbo_usp_add_bookTest_TestAction");
+            // 
+            // dbo_usp_add_bookTestData
+            // 
+            this.dbo_usp_add_bookTestData.PosttestAction = null;
+            this.dbo_usp_add_bookTestData.PretestAction = null;
+            this.dbo_usp_add_bookTestData.TestAction = dbo_usp_add_bookTest_TestAction;
+            // 
+            // notEmptyResultSetCondition1
+            // 
+            notEmptyResultSetCondition1.Enabled = true;
+            notEmptyResultSetCondition1.Name = "notEmptyResultSetCondition1";
+            notEmptyResultSetCondition1.ResultSet = 1;
+        }
+
+        #endregion
+
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        #endregion
+
+        [TestMethod()]
+        public void dbo_usp_add_bookTest()
+        {
+            SqlDatabaseTestActions testActions = this.dbo_usp_add_bookTestData;
+            // Execute the pre-test script
+            // 
+            System.Diagnostics.Trace.WriteLineIf((testActions.PretestAction != null), "Executing pre-test script...");
+            SqlExecutionResult[] pretestResults = TestService.Execute(this.PrivilegedContext, this.PrivilegedContext, testActions.PretestAction);
+            try
+            {
+                // Execute the test script
+                // 
+                System.Diagnostics.Trace.WriteLineIf((testActions.TestAction != null), "Executing test script...");
+                SqlExecutionResult[] testResults = TestService.Execute(this.ExecutionContext, this.PrivilegedContext, testActions.TestAction);
+            }
+            finally
+            {
+                // Execute the post-test script
+                // 
+                System.Diagnostics.Trace.WriteLineIf((testActions.PosttestAction != null), "Executing post-test script...");
+                SqlExecutionResult[] posttestResults = TestService.Execute(this.PrivilegedContext, this.PrivilegedContext, testActions.PosttestAction);
+            }
+        }
+        private SqlDatabaseTestActions dbo_usp_add_bookTestData;
+    }
+}
