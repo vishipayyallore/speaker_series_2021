81cb58321ac1a267179055ba35e446c38b93867c
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-05-28 20:54:52 +0530

    Initial Version

---
+++ CSharpCorner/28052021_Api_gRPC_Ng/Documentation/StarterFiles/5gRPCClients/2WindowsFormClient/CollegeGrpc.WindowsFormClient/FormAddNewProfessor.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace CollegeGrpc.WindowsFormClient
+{
+    partial class FormAddNewProfessor
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxTeaches = new System.Windows.Forms.TextBox();
+            this.textBoxSalary = new System.Windows.Forms.TextBox();
+            this.buttonFind = new System.Windows.Forms.Button();
+            this.checkBoxIsPhd = new System.Windows.Forms.CheckBox();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxProfessorId = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label1.Location = new System.Drawing.Point(19, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label2.Location = new System.Drawing.Point(6, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Teaches :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label3.Location = new System.Drawing.Point(16, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Salary :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label4.Location = new System.Drawing.Point(18, 176);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 23);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Is Phd :";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.ForeColor = System.Drawing.Color.Navy;
+            this.textBoxName.Location = new System.Drawing.Point(89, 52);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(274, 27);
+            this.textBoxName.TabIndex = 4;
+            // 
+            // textBoxTeaches
+            // 
+            this.textBoxTeaches.ForeColor = System.Drawing.Color.Navy;
+            this.textBoxTeaches.Location = new System.Drawing.Point(89, 95);
+            this.textBoxTeaches.Name = "textBoxTeaches";
+            this.textBoxTeaches.Size = new System.Drawing.Size(274, 27);
+            this.textBoxTeaches.TabIndex = 5;
+            this.textBoxTeaches.Text = "CSharp, Java";
+            // 
+            // textBoxSalary
+            // 
+            this.textBoxSalary.ForeColor = System.Drawing.Color.Navy;
+            this.textBoxSalary.Location = new System.Drawing.Point(89, 138);
+            this.textBoxSalary.Name = "textBoxSalary";
+            this.textBoxSalary.Size = new System.Drawing.Size(274, 27);
+            this.textBoxSalary.TabIndex = 7;
+            this.textBoxSalary.Text = "1234.56";
+            // 
+            // buttonFind
+            // 
+            this.buttonFind.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonFind.ForeColor = System.Drawing.Color.DarkBlue;
+            this.buttonFind.Location = new System.Drawing.Point(25, 213);
+            this.buttonFind.Name = "buttonFind";
+            this.buttonFind.Size = new System.Drawing.Size(167, 50);
+            this.buttonFind.TabIndex = 8;
+            this.buttonFind.Text = "Find Professor";
+            this.buttonFind.UseVisualStyleBackColor = true;
+            this.buttonFind.Click += new System.EventHandler(this.buttonFind_Click);
+            // 
+            // checkBoxIsPhd
+            // 
+            this.checkBoxIsPhd.AutoSize = true;
+            this.checkBoxIsPhd.Location = new System.Drawing.Point(89, 181);
+            this.checkBoxIsPhd.Name = "checkBoxIsPhd";
+            this.checkBoxIsPhd.Size = new System.Drawing.Size(18, 17);
+            this.checkBoxIsPhd.TabIndex = 10;
+            this.checkBoxIsPhd.UseVisualStyleBackColor = true;
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonClose.ForeColor = System.Drawing.Color.DarkBlue;
+            this.buttonClose.Location = new System.Drawing.Point(219, 213);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(167, 50);
+            this.buttonClose.TabIndex = 9;
+            this.buttonClose.Text = "Close";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label5.Location = new System.Drawing.Point(50, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 23);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Id :";
+            // 
+            // textBoxProfessorId
+            // 
+            this.textBoxProfessorId.ForeColor = System.Drawing.Color.Navy;
+            this.textBoxProfessorId.Location = new System.Drawing.Point(89, 9);
+            this.textBoxProfessorId.Name = "textBoxProfessorId";
+            this.textBoxProfessorId.Size = new System.Drawing.Size(308, 27);
+            this.textBoxProfessorId.TabIndex = 12;
+            // 
+            // FormAddNewProfessor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(409, 273);
+            this.Controls.Add(this.textBoxProfessorId);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.checkBoxIsPhd);
+            this.Controls.Add(this.buttonFind);
+            this.Controls.Add(this.textBoxSalary);
+            this.Controls.Add(this.textBoxTeaches);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormAddNewProfessor";
+            this.Text = "Add New Professor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.BindingSource tubeDataBindingSource;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxTeaches;
+        private System.Windows.Forms.TextBox textBoxSalary;
+        private System.Windows.Forms.Button buttonFind;
+        private System.Windows.Forms.CheckBox checkBoxIsPhd;
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxProfessorId;
+    }
+}
+
