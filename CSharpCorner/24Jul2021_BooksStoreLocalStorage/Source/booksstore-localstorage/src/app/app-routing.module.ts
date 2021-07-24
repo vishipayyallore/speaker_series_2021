3af8842fef64ebcb4335457553024211fc7c3036
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-07-24 10:40:56 +0530

    Added Menu and other screens

--- CSharpCorner/24Jul2021_BooksStoreLocalStorage/Source/booksstore-localstorage/src/app/app-routing.module.ts
+++ CSharpCorner/24Jul2021_BooksStoreLocalStorage/Source/booksstore-localstorage/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AboutusComponent } from './components/home/aboutus/aboutus.component';
+import { ContactusComponent } from './components/home/contactus/contactus.component';
+import { DashboardComponent } from './components/home/dashboard/dashboard.component';
+import { PageNotfoundComponent } from './components/shared/page-notfound/page-notfound.component';
+import { ListBooksComponent } from './components/books/list-books/list-books.component';
+
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'aboutus', component: AboutusComponent },
+  { path: 'contactus', component: ContactusComponent },
+  { path: 'list-books', component: ListBooksComponent },
+  { path: 'pagenotfound', component: PageNotfoundComponent },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: '**', redirectTo: 'pagenotfound', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- CSharpCorner/24Jul2021_BooksStoreLocalStorage/Source/booksstore-localstorage/src/app/app.module.ts
+++ CSharpCorner/24Jul2021_BooksStoreLocalStorage/Source/booksstore-localstorage/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopNavbarComponent } from './components/shared/top-navbar/top-navbar.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { PageNotfoundComponent } from './components/shared/page-notfound/page-notfound.component';
+import { DashboardComponent } from './components/home/dashboard/dashboard.component';
+import { AboutusComponent } from './components/home/aboutus/aboutus.component';
+import { ContactusComponent } from './components/home/contactus/contactus.component';
+import { ListBooksComponent } from './components/books/list-books/list-books.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavbarComponent,
+    FooterComponent,
+    PageNotfoundComponent,
+    DashboardComponent,
+    AboutusComponent,
+    ContactusComponent,
+    ListBooksComponent
   ],
   imports: [
     BrowserModule,
