75c92f97dda4fdced6f7a7a5fdf32d016284ac58
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-08-19 10:16:11 +0530

    Web API

---
+++ DotNetLearnersHouse/21Aug_.NET6_SPA/source/2restapi/College.API/Controllers/ProfessorsController.cs
@@ -0,0 +1,42 @@
+﻿using College.Core.Entities;
+using College.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace College.API.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfessorsController : ControllerBase
+    {
+        private readonly ILogger<ProfessorsController> _logger;
+        private readonly IProfessorsSqlBll _professorsSqlBll;
+
+        public ProfessorsController(ILogger<ProfessorsController> logger, IProfessorsSqlBll professorsSqlBll)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            
+            _professorsSqlBll = professorsSqlBll ?? throw new ArgumentNullException(nameof(professorsSqlBll));
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Professor>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Professor>>> Get()
+        {
+            _logger.Log(LogLevel.Debug, "Request Received for ProfessorsController::Get");
+
+            IEnumerable<Professor> professors = await _professorsSqlBll.GetAllProfessors();
+
+            _logger.Log(LogLevel.Debug, "Returning the results from ProfessorsController::Get");
+
+            return Ok(professors);
+        }
+
+    }
+
+}
--- DotNetLearnersHouse/21Aug_.NET6_SPA/source/2restapi/College.API/Program.cs
+++ DotNetLearnersHouse/21Aug_.NET6_SPA/source/2restapi/College.API/Program.cs
@@ -1,16 +1,12 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace College.API
 {
+
     public class Program
     {
+
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
@@ -22,5 +18,7 @@ namespace College.API
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
     }
+
 }
