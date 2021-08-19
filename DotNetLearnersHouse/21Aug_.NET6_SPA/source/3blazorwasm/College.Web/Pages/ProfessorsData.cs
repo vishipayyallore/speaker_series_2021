df5138742daf38b0d6ad713c9ecb1c24b27e8eae
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-08-20 00:16:41 +0530

    Today's work

---
+++ DotNetLearnersHouse/21Aug_.NET6_SPA/source/3blazorwasm/College.Web/Pages/ProfessorsData.cs
@@ -0,0 +1,27 @@
+﻿using College.Core.Entities;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace College.Web.Pages
+{
+
+    public partial class ProfessorsData
+    {
+        [Inject]
+        private HttpClient _httpClient { get; set; }
+        const string professorsEndPoint = "https://localhost:5001/api/professors";
+
+        public IEnumerable<Professor> Professors { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Professors = await JsonSerializer.DeserializeAsync<IEnumerable<Professor>>
+                    (await _httpClient.GetStreamAsync($"{professorsEndPoint}"),
+                    new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+        }
+    }
+
+}
