fadbbb77880977f0ed2e90eb09a27497ba0fe9a7
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-08-23 09:42:59 +0530

    Today's work

---
+++ DotNetLearnersHouse/21Aug_.NET6_SPA/source/database/college-datastore/dbo/Tables/Professors.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Professors]
+(
+	[ProfessorId] [uniqueidentifier] NOT NULL DEFAULT newid(),
+	[Name] [varchar](100) NULL,
+	[Doj] [datetime] NULL,
+	[Teaches] [varchar](100) NULL,
+	[Salary] [numeric](18, 2) NULL,
+	[IsPhd] [bit] NULL, 
+    CONSTRAINT [PK_Professors] PRIMARY KEY ([ProfessorId]),
+)
