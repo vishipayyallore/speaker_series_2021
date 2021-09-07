fa0ed4a4e34f78d3824c98b0c3e78535cb3d79f1
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-09-07 21:53:19 +0530

    Update a.sh

--- MicrosoftReactor/15Sep2021_AppServiceWebApps/infrastructure/WebAppsAzureCLI/a.sh
+++ MicrosoftReactor/15Sep2021_AppServiceWebApps/infrastructure/WebAppsAzureCLI/a.sh
@@ -1,36 +1,38 @@
-#!/bin/bash
-
-webAppName=azuremol$RANDOM
 SUBSCRIPTION="SwamyPKV VSPS"
-RESOURCEGROUP="rg-msft-reactor-1509-2021"
+RESOURCEGROUP="rg-az204-webapps-reactor-001"
 LOCATION="eastus"
-PLANNAME="plan-collegeapi"
+PLANNAME="plan-azsxdcfv"
 PLANSKU="F1"
-SITENAME="app-collegeapi"
-RUNTIME="DOTNETCORE|6.0"
+SITENAME="app-azsxdcfv"
+
+az login
+
+az account list -o table
+
+az account set --subscription $SUBSCRIPTION
 
 az group create --name $RESOURCEGROUP --location $LOCATION
 
-az appservice plan create \
-    --resource-group $RESOURCEGROUP \
-    --name $PLANNAME \
-    --sku $PLANSKU
+az appservice plan create --name $PLANNAME --location $LOCATION --sku $PLANSKU --resource-group $RESOURCEGROUP
 
-az webapp create \
-    --resource-group $RESOURCEGROUP \
-    --name $webAppName \
-    --plan $PLANNAME \
-    --deployment-local-git
+az webapp create --name $SITENAME --plan $PLANNAME --resource-group $RESOURCEGROUP
 
-git init && git add . && git commit -m "Initial Commit"
+# To set up deployment from a local git repository, uncomment the following commands.
+# first, set the username and password (use environment variables!)
+# USERNAME=""
+# PASSWORD=""
+# az webapp deployment user set --user-name $USERNAME --password $PASSWORD
 
-git remote add azure $(az webapp deployment source config-local-git \
-    --resource-group $RESOURCEGROUP \
-    --name $webAppName -o tsv)
+# now, configure the site for deployment. in this case, we will deploy from the local git repository
+# you can also configure your site to be deployed from a remote git repository or set up a CI/CD workflow
+az webapp deployment source config-local-git --name $SITENAME --resource-group $RESOURCEGROUP
 
+# the previous command returned the git remote to deploy to
+# use this to set up a new remote named "azure"
+git remote add azure "https://$USERNAME@$SITENAME.scm.azurewebsites.net/$SITENAME.git"
+
+# push master to deploy the site
 git push azure master
 
-hostName=$(az webapp show --resource-group $RESOURCEGROUP --name $webAppName --query defaultHostName --output tsv)
-
-# Now you can access the Web App in your web browser
-echo "To see your Web App in action, enter the following address in to your web browser:" $hostName
+# browse to the site
+az webapp browse --name $SITENAME --resource-group $RESOURCEGROUP
