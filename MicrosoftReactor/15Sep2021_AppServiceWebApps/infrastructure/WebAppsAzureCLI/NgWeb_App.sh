5e01983ae4d04cd2f37b730e57a266cf81aba0cd
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-09-08 23:35:44 +0530

    Update NgWeb_App.sh

--- MicrosoftReactor/15Sep2021_AppServiceWebApps/infrastructure/WebAppsAzureCLI/NgWeb_App.sh
+++ MicrosoftReactor/15Sep2021_AppServiceWebApps/infrastructure/WebAppsAzureCLI/NgWeb_App.sh
@@ -31,8 +31,8 @@ az webapp deployment user set --user-name $USERNAME --password $PASSWORD
 az webapp deployment user set --user-name "$webdeployuser" --password ""
 az webapp deployment source config-local-git --name "app-azsxdcfv2" --resource-group "rg-az204-webapps-reactor-001"
 
-git clone https://$app-azsxdcfv2@app-azsxdcfv2.scm.azurewebsites.net/app-azsxdcfv2.git
-git remote add azure https://$app-azsxdcfv2@app-azsxdcfv2.scm.azurewebsites.net/app-azsxdcfv.git
+git clone https://$USERNAME@app-azsxdcfv2.scm.azurewebsites.net/app-azsxdcfv2.git
+git remote add azure https://$USERNAME@app-azsxdcfv2.scm.azurewebsites.net/app-azsxdcfv.git
 
 # now, configure the site for deployment. in this case, we will deploy from the local git repository
 # you can also configure your site to be deployed from a remote git repository or set up a CI/CD workflow
@@ -41,15 +41,15 @@ az webapp deployment source config-local-git --name $SITENAME --resource-group $
 az webapp deployment user set --user-name $webdeployuser --password Sample123$
 
 az webapp deployment source config-local-git --name "app-azsxdcfv" --resource-group "rg-az204-webapps-reactor-001"
-git clone https://$app-azsxdcfv@app-azsxdcfv.scm.azurewebsites.net/app-azsxdcfv.git
-git remote add azure https://$app-azsxdcfv@app-azsxdcfv.scm.azurewebsites.net/app-azsxdcfv.git
+git clone https://$USERNAME@app-azsxdcfv.scm.azurewebsites.net/app-azsxdcfv.git
+git remote add azure https://$USERNAME@app-azsxdcfv.scm.azurewebsites.net/app-azsxdcfv.git
 
 Output: https://$webdeployuser@app-aassddff.scm.azurewebsites.net/app-aassddff.git
 # the previous command returned the git remote to deploy to
 # use this to set up a new remote named "azure"
 git remote add azure "https://$USERNAME@$SITENAME.scm.azurewebsites.net/$SITENAME.git"
 
-git remote add azure https://$app-azsxdcfv@app-azsxdcfv.scm.azurewebsites.net/app-azsxdcfv.git
+git remote add azure https://$USERNAME@app-azsxdcfv.scm.azurewebsites.net/app-azsxdcfv.git
 
 # push master to deploy the site
 git push azure master
