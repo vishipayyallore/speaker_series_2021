5ccb14f5c125a9b957a9790f6058402ed695c08f
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-09-28 12:11:21 +0530

    Update CreateWebApp.ps1

--- MicrosoftReactor/29-09-2021_AzureSQL_WebAppLogs/infrastructure/PowerShell/CreateWebApp.ps1
+++ MicrosoftReactor/29-09-2021_AzureSQL_WebAppLogs/infrastructure/PowerShell/CreateWebApp.ps1
@@ -8,7 +8,7 @@ $tier = 'Basic' # 'Free'
 New-AzResourceGroup -Name $rgname -Location $location
 
 # Create an App Service plan in S1 tier
-New-AzAppServicePlan -Name $webappname -Location $location -ResourceGroupName $rgname -Tier $tier
+New-AzAppServicePlan -Name $webappname -Location $location -ResourceGroupName $rgname -Linux -Tier $tier
 
 # Create a web app
 New-AzWebApp -Name $webappname -Location $location -AppServicePlan $webappname -ResourceGroupName $rgname
