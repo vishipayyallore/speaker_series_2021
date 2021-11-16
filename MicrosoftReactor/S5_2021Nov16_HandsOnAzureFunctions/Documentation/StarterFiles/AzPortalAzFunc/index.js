7488d1aadb7a9cd9ebdeaf995c040fe715acad6f
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-11-16 16:58:21 +0530

    Today's changes

--- MicrosoftReactor/S5_2021Nov16_HandsOnAzureFunctions/Documentation/StarterFiles/AzPortalAzFunc/index.js
+++ MicrosoftReactor/S5_2021Nov16_HandsOnAzureFunctions/Documentation/StarterFiles/AzPortalAzFunc/index.js
@@ -11,11 +11,16 @@ module.exports = async function (context, req) {
     if (inputs) {
         name = req.body.name;
         digits = req.body.digits;
+    } else {
+        name = 'No Name';
     }
 
     digits.forEach(digit => context.log(digit));
 
     context.log(`Returning the output`);
+
+    // Writing in Table Storage
+    context.bindings.outputTable = [];
 
     context.res = (inputs) ? {
         status: 200,
@@ -23,6 +28,7 @@ module.exports = async function (context, req) {
             success: true,
             message: `Hello, ${name} welcome to Array Operations.`,
             executedAt: process.env["COMPUTERNAME"],
+            executedOn: new Date().toISOString(),
             reverseData: _.reverse(digits),
             partitionData: _.partition(digits, n => n % 2)
         }
@@ -30,10 +36,23 @@ module.exports = async function (context, req) {
         status: 400,
         body: {
             success: false,
+            executedAt: process.env["COMPUTERNAME"],
+            executedOn: new Date().toISOString(),
             message: "Please Pass a 'name' and 'data' in the request body for a response."
         }
     };
+
+    context.bindings.outputTable.push({
+        PartitionKey: name,
+        RowKey: uuidv4(),
+        OutputStatus: context.res.status,
+        OutputData: context.res.body,
+    });
 }
 
-    // context.log(`${req.query?.digits} :: ${req.body?.name}`);
-    // context.log(`${req.body?.digits} :: ${req.body?.name}`);
+function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
---
+++ MicrosoftReactor/S5_2021Nov16_HandsOnAzureFunctions/Documentation/StarterFiles/AzPortalAzFuncBindings/httpindex.js
@@ -0,0 +1,15 @@
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    const name = (req.query.name || (req.body && req.body.name));
+    const responseMessage = name
+        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+
+    context.bindings.outputSbMsg = req.body;
+
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: responseMessage
+    };
+}
